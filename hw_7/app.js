235ba74a1bf7f238e1101850cf37deb7b6da9044
serhii_iklenko <sergeyiklenko@gmail.com>
2020-11-14 22:40:23 +0200

    hw 7

--- hw_7/app.js
+++ hw_7/app.js
@@ -53,52 +53,23 @@ assignObjects(obj1, obj2, ....., objn);
 
 
 function assignObjectsN(...objn) {
-    var obj = Object.assign(...objn);
-    return obj;
+    return object = Object.assign(...objn);
 }
 console.log(assignObjectsN({ x: 10, y: 20 }, { z: 30, d: 12 }, { a: 15 }, { b: 35 }, { c: 45 }));
 
 
-
-// function assignObjects2(...objn) {
-//     var obj1 = {};
-
-//     for (var key in objn) {
-//         obj1[key] = objn[key];
-//         // var obj2 = {};
-//         // for (var key in obj1) {
-//         //     obj2[key] = obj1[key];
-//         // }
-//         // for (var key in obj1) {
-//         //     var obj2 = {};
-//         //     if (typeof obj1[key] === 'object') {
-//         //         convert(obj1[key]);
-//         //     } else {
-//         //         obj2[key] = obj1[key];
-//         //     }
-//         // }
-
-//     }
-//     return obj1
-// }
-// console.log(assignObjects2({ x: 10, y: 20 }, { z: 30, d: 12 }, { a: 15 }, { b: 35 }, { c: 45 }));
-
-
 function assignObjects2(...objn) {
     var obj = {};
-    // var obj1 = {};
 
-    for (var key in objn) {
-        obj[key] = objn[key];
-        // for (var key in obj) {
-        //     if (typeof obj[key] === 'object') {
-        //         convert(obj[key]);
-        //     } else {
-        //         obj1[key] = obj[key]
-        //     }
-        // }
+    for (let index = 0; index < objn.length; index++) {
+        for (var key in objn[index]) {
+            if (objn[index].hasOwnProperty(key)) {
+                obj[key] = objn[index][key];
+            }
+        }
     }
+    console.log(obj)
     return obj
 }
-console.log(assignObjects2({ x: 10, y: 20 }, { z: 30, d: 12 }, { a: 15 }, { b: 35 }, { c: 45 }));
+assignObjects2({ x: 10, y: 20 }, { z: 30, d: 12 }, { a: 15 }, { b: 35 }, { c: 45 });
 
