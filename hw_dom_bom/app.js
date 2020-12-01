e0d6a4734eb1023db810ca3dbb5549c28099c607
serhii_iklenko <sergeyiklenko@gmail.com>
2020-12-01 23:03:39 +0200

    fix

--- hw_dom_bom/app.js
+++ hw_dom_bom/app.js
@@ -1,15 +1,20 @@
 // const inputs = document.querySelectorAll('input');
 // const inputs = document.getElementsByTagName("input");
 
-const input_1 = document.getElementById('inp1').value;
-const input_2 = document.getElementById('inp2').value;
-const input_3 = document.getElementById('inp3').value;
+const input_1 = document.getElementById('inp1');
+const input_2 = document.getElementById('inp2');
+const input_3 = document.getElementById('inp3');
 
 const textArea = document.querySelector('textarea');
 
-
-setInterval(() => {
-    if (textArea.value = input_1) {
-        textArea.value += ', ' + input_2 + ', ' + input_3;
+setInterval(input_1.onblur = function () {
+    if (textArea.value = input_1.value) {
+        textArea.value += ', ' + input_2.value + ', ' + input_3.value;
     }
 }, 3000)
+
+
+
+
+
+
